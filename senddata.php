2299481f104e78a994e6604ff3f744df11f6534d
mikung <mikung555@gmail.com>
2017-02-02 21:31:49 +0700

    first commit

--- senddata.php
+++ senddata.php
@@ -5,7 +5,7 @@
  * Date: 02/02/2560
  * Time: 21:26
  */
-require __DIR__."vendor/autoload.php";
+require "vendor/autoload.php";
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('DqC0E6bwB9GJjUsZdCdyhtC1b6KXlp/DXoBnzPbt1/v+z0p0FCzHk5XAbO9nm2HQL8AoawkXmbJmMabvXfKrdJeueUzpp27IKe8kDox3Y4U2hjOjsM2l32hIc47h7TrPHrhtfeRQyXLwALD3yP1EJAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ec68c63b493064bedb0c8eccf7f328d6']);
