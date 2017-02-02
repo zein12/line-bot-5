5511475f3b1ff6dd5b789e68b078895b4a347b87
mikung <mikung555@gmail.com>
2017-02-02 21:44:26 +0700

    first commit

--- senddata.php
+++ senddata.php
@@ -10,8 +10,40 @@ require "vendor/autoload.php";
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('DqC0E6bwB9GJjUsZdCdyhtC1b6KXlp/DXoBnzPbt1/v+z0p0FCzHk5XAbO9nm2HQL8AoawkXmbJmMabvXfKrdJeueUzpp27IKe8kDox3Y4U2hjOjsM2l32hIc47h7TrPHrhtfeRQyXLwALD3yP1EJAdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ec68c63b493064bedb0c8eccf7f328d6']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('นายมิคุง');
+//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('นายมิคุง');
 
-$response = $bot->pushMessage('U90d4da92752f6e692797e75d993d0d6e', $textMessageBuilder);
-
+$template = '{
+    "type": "template",
+  "altText": "this is a confirm template",
+  "template": {
+        "type": "confirm",
+      "text": "Are you sure?",
+      "actions": [
+          {
+              "type": "message",
+            "label": "Yes",
+            "text": "yes"
+          },
+          {
+              "type": "message",
+            "label": "No",
+            "text": "no"
+          }
+      ]
+  }
+}';
+$text = 'จะมาประชุมมัย';
+$action = '[{
+              "type": "message",
+            "label": "Yes",
+            "text": "yes"
+          },
+          {
+              "type": "message",
+            "label": "No",
+            "text": "no"
+          }]';
+$templatebuilder =  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder($text,$action);
+//$response = $bot->pushMessage('U90d4da92752f6e692797e75d993d0d6e', $textMessageBuilder);
+$response = $bot->pushMessage('U90d4da92752f6e692797e75d993d0d6e', $templatebuilder);
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
