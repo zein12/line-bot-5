8b6cea09393ecbd50d776404bdb06fc21bb27cfa
mikung <mikung555@gmail.com>
2017-02-02 22:18:17 +0700

    first commit

---
+++ vendor/image.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mikung
+ * Date: 02/02/2560
+ * Time: 21:26
+ */
+require "vendor/autoload.php";
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('DqC0E6bwB9GJjUsZdCdyhtC1b6KXlp/DXoBnzPbt1/v+z0p0FCzHk5XAbO9nm2HQL8AoawkXmbJmMabvXfKrdJeueUzpp27IKe8kDox3Y4U2hjOjsM2l32hIc47h7TrPHrhtfeRQyXLwALD3yP1EJAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'ec68c63b493064bedb0c8eccf7f328d6']);
+
+
+$message = json_encode($action);
+//$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('data',$message);
+$imagemessage = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder('https://cdn.pixabay.com/photo/2013/07/12/15/04/monitor-149362__340.png','https://cdn.pixabay.com/photo/2013/07/12/15/04/monitor-149362__340.png');
+$response = $bot->pushMessage('U90d4da92752f6e692797e75d993d0d6e', $textMessageBuilder);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
